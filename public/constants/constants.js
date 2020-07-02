7d0df2cd6ffbbc15bc49491f2e4ffc747e431d8f
Nancy M Schorr <nms@schorrmedia.com>
2020-07-03 01:30:11 +0200

    new files

---
+++ public/constants/constants.js
@@ -0,0 +1,17 @@
+var CHAINID = 4810;
+var PW = "kathy123"
+var CONT_ADDY = "SPEXdKRT56gn8YfFy8DjSgQbkoVaTkn6XxrLL7";
+var SENDER =   "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+var OWNER =  "SPEXdKRT4pz7ZhasM9pTK4fvGrJf8eod5ZqtXa";
+var BUYER =   "SPEXdKRT4nswCgHUv1YfuC9DK2u8etwpPbx8Np";
+
+var VALUE_ASSET = 25;
+var GAS_PRICE = 25;
+var GAS_LIMIT = 9000000;
+
+var POSTURL_w3 = "http://westteam.nulstar.com:8003";
+var POSTURL_w4 = "http://westteam.nulstar.com:8004/jsonrpc";
+
+export var constantsObj = {
+    CHAINID, PW, CONT_ADDY, SENDER, OWNER, BUYER, 
+    VALUE_ASSET, GAS_PRICE, GAS_LIMIT, POSTURL_w3, POSTURL_w4 }
