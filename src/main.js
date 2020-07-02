d68bff0fe37dc3ad73d1c8043b032d1aefbdf80f
Nancy M Schorr <nms@schorrmedia.com>
2020-07-03 01:59:58 +0200

    new changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
-import router from './router'
+//import store from './store'
+//import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  router,
+//  store,
+//  router,
   render: h => h(App)
 }).$mount('#app')
+
